57d316712dbf199353adf72871859124bcbadb1c
wankun-github <1278795705@qq.com>
2017-06-28 17:46:16 +0800

    first

---
+++ src/main/java/com/hand/PrekyApplication.java
@@ -0,0 +1,12 @@
+package com.hand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrekyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrekyApplication.class, args);
+	}
+}
